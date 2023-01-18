68aae29e20a6c14b0e3518a0636329f8e57ab9ae
Ihor Neizvestnyi <ihor.neizvestnyi@novait.local>
2023-01-18 21:50:59 +0200

    fdfd

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,22 @@
 <template>
-  <div>Hello nuxt3</div>
+  <div>
+    <Navbar />
+    <main>
+      <div class="container">
+        <section>
+          <h1>This is home page!</h1>
+        </section>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue'
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  components: {
+    Navbar
+  }
 }
 </script>
